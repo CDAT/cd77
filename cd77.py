f7acf70c25232920d8c160684b0ef73e804f6770
Charles Doutriaux <doutriaux1@llnl.gov>
2016-03-07 13:49:45 -0800

    removed duplication cd77/cd77.py updated cd77.py for crunchy/linux/gfortran

--- cd77.py
+++ cd77.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import sys, os, string
+import cdat_info
 
 # Compile a Fortran EzGet/LATS/CDMS application
 
@@ -20,31 +22,33 @@ Examples:
   cd77 -ezget -o ezsample ezsample.F
   cd77 -lats -verbose -o lats_sample lats_sample.f
 """
-
+cdat_location = sys.prefix+"/Externals"
 # Locations of libraries and include files on the various platforms
 
 ezgetlib = {"irix6" : "-L/pcmdi/ktaylor/pcmdi/util/ezget/Sgi6.2 -lezget",
             "sunos5" : "-L/pcmdi/cirrus/ktaylor/ezget/solaris -lezget",
-            "linux2" : "-L/usr/local/lib -lezget"}
-cdmsinc = {"irix6" : "-I/usr/local/include -I/usr/local/HDF4.0r2/include",
-           "sunos5" : "-I/usr/local/include",
-           "linux2" : "-I/usr/local/include"}
-cdmslib = {"irix6" : "-L/usr/local/lib -lcdms -lnetcdf -L/usr/local/HDF4.0r2/lib -lmfhdf -ldf -ljpeg -lz -ldrs",
-           "sunos5" : "-L/usr/local/lib -lcdms -lmfhdf -ldf -ljpeg -lz -lnetcdf -lnsl -ldrs -lm -lsunmath",
-           # "linux2" : "-L/usr/local/lib -lcdms -lnetcdf -lmfhdf -ldf -ljpeg -lz -lmfhdf -ldf -ljpeg -lz -ldrs -lm"
-           "linux2" : "-L/usr/local/lib -lcdms -lnetcdf -ldrs -lm"
+            "linux2" : "-L"+cdat_location+"/lib -lezget"}
+cdmsinc = {"irix6" : "-I"+cdat_location+"/include -I"+cdat_location+"/HDF4.0r2/include",
+           "sunos5" : "-I"+cdat_location+"/include",
+           "linux2" : "-I"+cdat_location+"/include"}
+cdmslib = {"irix6" : "-L"+cdat_location+"/lib -lcdms -lnetcdf -L"+cdat_location+"/HDF4.0r2/lib -lmfhdf -ldf -ljpeg -lz -ldrs",
+           "sunos5" : "-L"+cdat_location+"/lib -lcdms -lmfhdf -ldf -ljpeg -lz -lnetcdf -lnsl -ldrs -lm -lsunmath",
+           # "linux2" : "-L"+cdat_location+"/lib -ldrs -lcdms -lnetcdf -lmfhdf -ldf -ljpeg -lz -lmfhdf -ldf -ljpeg -lz -lm"
+           "linux2" : "-L"+cdat_location+"/lib  -ldrs -l"+" -l".join(cdat_info.cdunif_libraries)
            }
-latsinc = {"irix6" : "-I/usr/local/include",
-           "sunos5" : "-I/usr/local/include",
-           "linux2" : "-I/usr/local/include"}
-latslib = {"irix6" : "-L/usr/local/lib -llats -lnetcdf -lm",
-           "sunos5" : "-L/usr/local/lib -llats -lnetcdf -lnsl -lm",
-           "linux2" : "-L/usr/local/lib -llats -lnetcdf -lm"}
+latsinc = {"irix6" : "-I"+cdat_location+"/include",
+           "sunos5" : "-I"+cdat_location+"/include",
+           "linux2" : "-I"+cdat_location+"/include"}
+latslib = {"irix6" : "-L"+cdat_location+"/lib -llats -lnetcdf -lm",
+           "sunos5" : "-L"+cdat_location+"/lib -llats -lnetcdf -lnsl -lm",
+           "linux2" : "-L"+cdat_location+"/lib -llats -lnetcdf -lm"}
+
+f77lib = { "linux2" : "-lgfortran"}
+
 f77exec = {"irix6" : "f77",
            "sunos5" : "f77",
-           "linux2" : "pgf77"}
+           "linux2" : "gfortran"}
 
-import sys, os, string
 
 # Get the host type and major operating system version
 def getplatform():
@@ -100,8 +104,11 @@ def main(argv):
         sys.exit(1)
             
     # Build and execute the command
-    command = f77+" "+includes+f77opts+libs
+    command = f77+" "+includes+" "+f77opts+" "+f77lib[platform]+" "+libs
+    # -C not needed these days
+    command = command.replace(" -C "," ")
     if verbose:
+        print "UVCDAT LOCATION:",cdat_location
         print command
     os.system(command)
 
