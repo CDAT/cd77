ecf959522b7930fb330de25c3154a044e3ab2791
Charles Doutriaux <doutriaux1@llnl.gov>
2016-03-08 09:13:59 -0800

    simplified the whole thing, uses cdat_info instead

--- cd77.py
+++ cd77.py
@@ -23,50 +23,25 @@ Examples:
   cd77 -ezget -o ezsample ezsample.F
   cd77 -lats -verbose -o lats_sample lats_sample.f
 """
-cdat_location = sys.prefix + "/Externals"
+
+cdat_libs_locations = ""
+for l in cdat_info.cdunif_library_directories+["/usr/X11R6/lib",]:
+    cdat_libs_locations+=" -L"+l
+
+cdat_inc_locations = ""
+for l in cdat_info.cdunif_include_directories:
+    cdat_inc_locations+=" -I"+l
+
+
 # Locations of libraries and include files on the various platforms
 
-ezgetlib = {
-    #"irix6" : "-L/pcmdi/ktaylor/pcmdi/util/ezget/Sgi6.2 -lezget",
-    #"sunos5" : "-L/pcmdi/cirrus/ktaylor/ezget/solaris -lezget",
-    "linux2": "-L" + cdat_location + "/lib -lezget",
-    "darwin": "-L" + cdat_location + "/lib -lezget"}
-cdmsinc = {
-    #"irix6" : "-I"+cdat_location+"/include -I"+cdat_location+"/HDF4.0r2/include",
-    #"sunos5" : "-I"+cdat_location+"/include",
-    "linux2": "-I" + cdat_location + "/include",
-    "darwin": "-I" + cdat_location + "/include"}
-cdmslib = {
-    #"irix6" : "-L"+cdat_location+"/lib -lcdms -lnetcdf -L"+cdat_location+"/HDF4.0r2/lib -lmfhdf -ldf -ljpeg -lz -ldrs",
-    #"sunos5" : "-L"+cdat_location+"/lib -lcdms -lmfhdf -ldf -ljpeg -lz -lnetcdf -lnsl -ldrs -lm -lsunmath",
-    # "linux2" : "-L"+cdat_location+"/lib -ldrs -lcdms -lnetcdf -lmfhdf -ldf -ljpeg -lz -lmfhdf -ldf -ljpeg -lz -lm"
-    "linux2": "-L" + cdat_location + "/lib  -ldrs -l" + " -l".join(cdat_info.cdunif_libraries),
-    "darwin": "-L" + cdat_location + "/lib  -ldrs -l" + " -l".join(cdat_info.cdunif_libraries)
-}
-latsinc = {
-    #"irix6" : "-I"+cdat_location+"/include",
-    #"sunos5" : "-I"+cdat_location+"/include",
-    "linux2": "-I" + cdat_location + "/include",
-    "darwin": "-I" + cdat_location + "/include"}
-latslib = {
-    #"irix6" : "-L"+cdat_location+"/lib -llats -lnetcdf -lm",
-    #"sunos5" : "-L"+cdat_location+"/lib -llats -lnetcdf -lnsl -lm",
-    "linux2": "-L" + cdat_location + "/lib -llats -lnetcdf -lm",
-    "darwin": "-L" + cdat_location + "/lib -llats -lnetcdf -lm"}
+ezgetlib =  "-lezget"
+cdmslib = "-ldrs -l" + " -l".join(cdat_info.cdunif_libraries)
+latslib = "-llats -lnetcdf -lm"
+f77lib = "-lgfortran"
+         
 
-f77lib = {"linux2": "-lgfortran",
-          "darwin": "-lgfortran"}
-
-f77exec = {
-    #"irix6" : "f77",
-    #"sunos5" : "f77",
-    "linux2": "gfortran",
-    "darwin": "gfortran"}
-
-
-# Get the host type and major operating system version
-def getplatform():
-    return sys.platform
+f77exec = "gfortran"
 
 
 def main(argv):
@@ -75,7 +50,7 @@ def main(argv):
     lats = 0
     verbose = 0
     includes = ""
-    libs = ""
+    libs = f77lib
 
     f77opts = "-g"
 
@@ -98,30 +73,21 @@ def main(argv):
         print usage
         sys.exit(0)
 
-    # Get the platform and include, lib directives
-    # for this platform
-    platform = getplatform()
-    try:
-        f77 = f77exec[platform]
-        if ezget:
-            libs = libs + ' ' + ezgetlib[platform]
-        if cdms:
-            includes = includes + ' ' + cdmsinc[platform]
-            libs = libs + ' ' + cdmslib[platform]
-        if lats:
-            includes = includes + ' ' + latsinc[platform]
-            libs = libs + ' ' + latslib[platform]
-    except KeyError:
-        print "Platform not supported:", platform
-        sys.exit(1)
+    # Constructs the command line
+    if ezget:
+        libs = libs + ' ' + ezgetlib
+    if cdms:
+        libs = libs + ' ' + cdmslib
+    if lats:
+        libs = libs + ' ' + latslib
 
-    # Build and execute the command
-    command = f77 + " " + includes + " " + \
-        f77opts + " " + f77lib[platform] + " " + libs
+    command = "%s %s %s %s %s" % (f77exec, f77opts, cdat_inc_locations, cdat_libs_locations, libs)
+    # Execute the command
+
     # -C not needed these days
     command = command.replace(" -C ", " ")
+
     if verbose:
-        print "UVCDAT LOCATION:", cdat_location
         print command
     os.system(command)
 
